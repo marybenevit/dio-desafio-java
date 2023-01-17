f8a8c8e67de69eca0a7db494434e8390adb5dcb9
Mariana Benevit <marybenevit@gmail.com>
2023-01-17 18:02:48 -0300

    "Inclusão das classes"

---
+++ desafio-dio-poo/src/Main.java
@@ -0,0 +1,28 @@
+import br.com.dio.desafio.dominio.Curso;
+import br.com.dio.desafio.dominio.Mentoria;
+
+import java.time.LocalDate;
+
+public class Main {
+    public static void main(String[] args) {
+
+        Curso curso = new Curso();
+        curso.setTitulo("Curso Java");
+        curso.setDescricao("Este é um curso de Java.");
+        curso.setCargaHoraria(8);
+
+        Curso curso1 = new Curso();
+        curso1.setTitulo("Curso Java");
+        curso1.setDescricao("Este é um curso de Java.");
+        curso1.setCargaHoraria(8);
+
+        Mentoria mentoria = new Mentoria();
+        mentoria.setTitulo("Mentoria de Java");
+        mentoria.setDescricao("Esta é uma mentoria de Java.");
+        mentoria.setData(LocalDate.now());
+
+        System.out.println(curso);
+        System.out.println(curso1);
+        System.out.println(mentoria);
+    }
+}
